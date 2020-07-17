739112ccdcf4981cfe531aea92fe1889f6f36ffb
GilliusThunderHead <20652958+GilliusThunderHead@users.noreply.github.com>
2020-07-17 19:45:09 +1000

    Models completed

--- products/models.py
+++ products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+import time
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Product Class
+class Product(models.Model):
+
+	title=models.CharField(max_length=50)
+	url=models.TextField()
+	pub_date=models.DateTimeField()
+	votes_total=models.IntegerField(default=1)
+	image=models.ImageField(upload_to='images/')
+	icon=models.ImageField(upload_to='images/')
+	body=models.TextField()
+	pub_date_pretty=models.DateTimeField()
+	hunter=models.ForeignKey(User,on_delete=models.CASCADE)
+#TODO
+#pub_date_pretty
+#hunter
+
+	def __str__(self):
+			return self.title
+		
+		def __str__(self):
+			return self.url
+		
+		def __str__(self):
+			return self.pub_date
+		
+		def __str__(self):
+			return self.votes_total
+
+		def __str__(self):
+			return self.image
+			
+		def __str__(self):
+			return self.icon
+
+		def summary(self):
+			return self.body[:200]
+			
+		def pub_date_pretty
+			return self.pub_date.strftime('%b %e %y')
